6c54a8d450f3eb9247f4f19587730b817a3980c6
hassanardebili <hardebilih@gmail.com>
2022-05-12 14:41:43 +0430

    app added

--- core/Application.php
+++ core/Application.php
@@ -8,6 +8,7 @@ class Application
     public Router $router;
     public Request $request;
     public Response $response;
+    public static Application $app ;
 
     public function __construct($root_path)
     {
@@ -15,6 +16,7 @@ class Application
         $this->request = new Request;
         $this->response = new Response;
         $this->router = new Router($this->request, $this->response);
+        self :: $app = $this;
     }
 
     public function get($path, $callback)
